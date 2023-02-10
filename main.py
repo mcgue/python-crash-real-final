113523be69c7c3e9e643ba8a9f6a5ad6ca4c3f22
mcgue <mcgu@protonmail.com>
2023-02-10 10:35:50 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
