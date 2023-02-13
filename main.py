47ec2ca22c5079ea6f83c94c2151726320e7eacc
mcgue <mcgu@protonmail.com>
2023-02-12 18:35:37 -0800

    add read file

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import fileupload
 import io
 import sys
 
+# Get file
+file_contents = open(r"dick.txt","r")
 
 def print_hi(name):
     print(f'Hi, {name}')
 
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(file_contents)
