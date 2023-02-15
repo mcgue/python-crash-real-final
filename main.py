00839e00eb0302c525289c317ead170b03497c77
mcgue <mcgu@protonmail.com>
2023-02-14 19:06:45 -0800

    change method to read text into a file

--- main.py
+++ main.py
@@ -2,17 +2,25 @@
 import wordcloud
 import numpy as np
 from matplotlib import pyplot as plt
-from IPython.display import display
-import fileupload
 import io
 import sys
 
-# Get file
-file_contents = open(r"dick.txt","r")
+# Open file and put text into file
+with open('dick.txt') as f:
+    contents = f.read()
+
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my",
+                       "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her",
+                       "hers", "its", "they", "them", "their", "what", "which", "who", "whom", "this",
+                       "that", "am", "are", "was", "were", "be", "been", "being", "have", "has", "had",
+                       "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
+                       "where", "how", "all", "any", "both", "each", "few", "more", "some", "such",
+                       "no", "nor", "too", "very", "can", "will", "just"]
+
 
 def print_hi(name):
     print(f'Hi, {name}')
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print(file_contents)
+    print(contents)
