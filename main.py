6cec5aa2975759cac14327a9ab8839d9d13e6a7c
mcgue <mcgu@protonmail.com>
2023-02-18 12:46:56 -0800

    specify encoding of text file so functions

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import io
 import sys
 
 # Open file and put text into file
-with open('dick.txt') as f:
+with open('moby-dick.txt', 'r', encoding='utf-8') as f:
     contents = f.read()
 
+# my_file = open("moby-dick.txt", "r", encoding='utf-8')
+# contents = my_file.read()
+
 contents = contents.replace('\n', ' ')
-contents = contents.replace('\n', ' ')
+# contents = contents.replace('.', ' ')
 
 # Create list
 contents_into_list = []
@@ -20,13 +23,13 @@ contents_into_list = []
 contents_into_list = contents.split(" ")
 
 punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my",
-                       "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her",
-                       "hers", "its", "they", "them", "their", "what", "which", "who", "whom", "this",
-                       "that", "am", "are", "was", "were", "be", "been", "being", "have", "has", "had",
-                       "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
-                       "where", "how", "all", "any", "both", "each", "few", "more", "some", "such",
-                       "no", "nor", "too", "very", "can", "will", "just"]
+# uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my",
+#                       "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her",
+#                       "hers", "its", "they", "them", "their", "what", "which", "who", "whom", "this",
+#                       "that", "am", "are", "was", "were", "be", "been", "being", "have", "has", "had",
+#                       "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
+#                       "where", "how", "all", "any", "both", "each", "few", "more", "some", "such",
+#                       "no", "nor", "too", "very", "can", "will", "just"]
 
 def print_hi(name):
     print(f'Hi, {name}')
